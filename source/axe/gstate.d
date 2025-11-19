b80dee2eb3207e29713a1b2cf8ab420e24dc70e7
Navid M <navidmomtahen@gmail.com>
2025-11-19 18:44:28 +0000

    add

---
+++ source/axe/gstate.d
@@ -0,0 +1,25 @@
+module axe.gstate;
+
+static class GlobalState
+{
+    static bool quietMode = true;
+}
+
+/** 
+ * Helper function for conditional debug output.
+ *
+ * Params:
+ *   args = Arguments to be printed if not in quiet mode
+ */
+void debugWriteln(Args...)(Args args)
+{
+    debug
+    {
+        if (!GlobalState.quietMode)
+        {
+            import std.stdio : writeln;
+
+            writeln(args);
+        }
+    }
+}
